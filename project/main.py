b920003e1e7e3946661359988dd1459a397abb35
Hyejin Kang <kkamboting@gmail.com>
2023-04-27 17:54:08 +0900

    0427_machinelearning_framewor만들기

--- project/main.py
+++ project/main.py
@@ -53,23 +53,97 @@ def train_and_test(epoch_count=10, mb_size=10):
         losses = []
         accs = []
         for n in range(step_count):
+            acc = run_test(test_x, test_y)
             train_x, train_y = get_train_data(mb_size, n)
-            loss, acc = run_train(train_x, train_y)
+            loss, acc = run_train(train_x, train_y) # 순전파
             losses.append(loss)
             accs.append(acc)
 
+        print(f"Epoch {epoch+1} : loss={np.mean(losses):5.3f}, accs={np.mean(accs):5.3f}, accs={np.mean(acc):5.3f}")
+
     # 3. 테스트
-    pass
+    final_acc = run_test(test_x, test_y)
+    print(f"Accs={np.mean(final_acc):5.3f}")
+
+def run_train(x, y):
+    # 순방향 => 정확도
+    output, aux_nn = forward_neuralnet(x)
+    loss, aux_pp = forward_postproc(output, y)
+    accuracy = eval_accuracy(output, y)
+
+    # 역방향 => 손실(오차)
+    G_loss = 1.0
+    G_output = backprop_postproc(G_loss, aux_pp)
+    backprop_neuralnet(G_output, aux_nn)  # 기울기와 절편 수정
+
+    return loss, accuracy
+
+
+def run_test(x, y):
+    output, aux_nn = forward_neuralnet(x)
+    accuracy = eval_accuracy(output, y)
+
+    return accuracy
+
+
+def forward_neuralnet(x):
+    # y = w1*x1 + b
+    global weight, bias
+    output = np.matmul(x, weight) + bias
+
+    return output, x
+
+
+def forward_postproc(output, y):
+    diff = output - y
+    square = np.square(diff)
+    loss = np.mean(square)
+
+    return loss, diff
+
+
+def eval_accuracy(output, y):
+    mdiff = np.mean(np.abs((output-y)/y))
+
+    return 1 - mdiff
+
+
+def backprop_postproc(G_loss, diff):
+
+    # 오차의 기울기 구하기
+    shape = diff.shape
+    g_loss_square = np.ones(shape) / np.prod(shape)
+    g_square_diff = 2 * diff
+    g_diff_output = 1
+
+    G_square = g_loss_square * G_loss
+    G_diff = g_square_diff * G_square
+    G_output = g_diff_output * G_diff
+
+    return G_output
+
+
+def backprop_neuralnet(G_output, aux_nn):
+
+    # 역전파 편미분
+    global weight, bias
+    g_output_w = aux_nn.transpose()
+
+    G_w = np.matmul(g_output_w, G_output)
+    G_b = np.sum(G_output, axis=0)
+
+    weight -= LEARNING_RATE * G_w
+    bias -= LEARNING_RATE * G_b
 
 
 def arrange_data(mb_size):
     global data, shuffle_map, test_begin_idx
-    shuffle_map = np.arange(data.shape[0]) # -> NDArray
+    shuffle_map = np.arange(data.shape[0])  # -> NDArray
     np.random.shuffle(shuffle_map)
     step_count = int(data.shape[0] * 0.8) // mb_size
     test_begin_idx = step_count * mb_size
 
-    return step_count  # mb_size에 의존
+    return step_count  # mb_size 의존
 
 
 def get_data_test():
@@ -89,24 +163,10 @@ def get_train_data(mb_size, nth):
     return train_data[:, :-output_cnt], train_data[:, -output_cnt:]
 
 
-def run_train(x, y):
-    # 순방향 => 정확도
-    output, aux_nn = forward_neuralnet(x)
-    loss, aux_pp = forward_postproc(output, y)
-    accuracy = eval_accuracy(output, y)
-
-    # 역방향 => 손실(오차)
-    G_loss = 1.0
-    G_output = backprop_postproc(G_loss, aux_pp)
-    backprop_neuralnet(G_output, aux_nn)
-
-    return loss, accuracy
-
-
 if __name__ == "__main__":
     # 입력
-    load_dataset() # CSV 파일 읽기
+    load_dataset()  # CSV 파일 읽기
     # 처리
-    init_model() # 기울기와 절편 배열 data[x,y]
+    init_model()  # 기울기와 절편 배열 data[x,y]
     # 출력
-    train_and_test() # epoch, batch_size
+    train_and_test()  # epoch, batch_size
